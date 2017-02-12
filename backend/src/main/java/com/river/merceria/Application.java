68d46bc202d062828b616b6bdd89eee5725ed025
Carlos Toranzo <carlos.t@ag2technology.com>
2017-02-12 20:37:54 -0300

    Change package

---
+++ backend/src/main/java/com/river/merceria/Application.java
@@ -0,0 +1,15 @@
+package com.river.merceria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by charly on 2/9/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
